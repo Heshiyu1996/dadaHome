bcc65df105476c402d588ea66ac8d1a364c38551
你的宇哥哥 <450979309@qq.com>
2017-07-29 19:33:32 +0800

    19:33

--- src/App.vue
+++ src/App.vue
@@ -44,20 +44,23 @@
     		</div>
     	</div>
 
-      <div class="css-body" id="Index">
-         <img class="css-body-bg" width="450px" height="350px"  src="./assets/img/ad_ipad.png" />
+      <div class="css-body " id="Index">
+        <img class="css-body-bg" src="./assets/img/bg1.png" />
+
          <div class="css-body-ad">
+
             <div class="css-body-ad-title">搭搭家</div>
             <div class="css-body-ad-engtitle">DADAHOME</div>
             <div class="css-body-ad-desc">搭搭家是一款SaaS模式的企业移动销售云服务展示软件系统，专门为企业提供更高效更优质的品牌营销，产品展示，网络传播，以及强大的产品即时搭配功能等服务。 </div>
+            <div class="css-body-ad-btn">
+              <a href="#">
+                <img  class="css-body-ad-btn-img" src="./assets/img/iphone.png" />
+                <div  class="css-body-ad-btn-desc">S E E &nbsp;&nbsp;O N &nbsp;&nbsp;A P P S T O R E</div>
+              </a>
+            </div>
         </div>
-        <div class="css-body-btn">
-          <a href="#">
-            <img  class="css-body-btn-img" src="./assets/img/iphone.png" />
-            <div  class="css-body-btn-desc">S E E &nbsp;&nbsp;O N &nbsp;&nbsp;A P P S T O R E</div>
-          </a>
-        </div>
-        <div class="clear"></div>
+        <!-- <img class="css-body-bg" src="./assets/img/ad_ipad.png" /> -->
+        <div class="clearfix"></div>
       </div>
     	<div class="css-intro" id="APP">
     		<div class="css-intro-title">
@@ -75,42 +78,45 @@
     	<div class="css-intro">
     		<div class="css-intro-title">
           <div style="flex:1"></div>
-          <div style="width:295px">
+          <div>
             <img class="css-intro-title-img" src="./assets/img/icon_Ask.png" />
       			<div class="css-intro-title-ask">搭搭家解决什么问题？</div>
           </div>
           <div style="flex:1"></div>
     		</div>
-        <div class="css-intro-content2" style="width:790px;">
-        			<div class="css-intro-content2-card">
-        				<div style="height:30%">
-                  <img class="css-intro-content2-card-icon" src="./assets/img/onCloud.png" />
-                  <div class="css-intro-content2-card-name">云端技术</div>
-                </div>
-        				<div class="css-intro-content2-card-desc extraH">
-                  大部分家具厂家依然停留在纸质媒介的传统传播手段，如图册，折页等，但非常遗憾，这种传统的传播途径明显已经跟不上移动网络时代传播的节奏和脚步，通过搭搭家APP系统的云端技术，网络传播，轻松打通所有客户渠道，所有的经销商，专卖店，只要下载一个软件，就能轻松开始做营销了!
-                </div>
-        			</div>
-        			<div class="css-intro-content2-card marginLf">
-        				<div style="height:30%">
-                  <img class="css-intro-content2-card-icon" src="./assets/img/onSpace.png" />
-                  <div class="css-intro-content2-card-name">海量空间</div>
-                </div>
-        				<div class="css-intro-content2-card-desc extraH" style="">
-                  每间家具厂家都有大量的产品图片需要处理，但大部分还是停留在简单的单张图片保存，整理效率和传播效率都非常低下，通过软件后台有序列地管理，大大提高整理效果和产品档案归类，并且能够通过网络瞬间传播，这是纸质媒介没办法达到的效果，而且云端海量空间，再多的图片也不用担心。
-                </div>
-        			</div>
-        			<div class="css-intro-content2-card marginLf">
-        				<div style="height:30%">
-                  <img class="css-intro-content2-card-icon" src="./assets/img/onUpdate.png" />
-                  <div class="css-intro-content2-card-name">灵活更新</div>
-                </div>
-        				<div class="css-intro-content2-card-desc" style="height:70%">
-                  每间家具厂，每年都会推出新产品，但是旧图册是没办法瞬间更新的，又只能重新排版，重新印刷，年复一年不断付出高成本的图册费用，软件则不存在这种问题，随时上传，随时更新，随时传播，随时编辑，而且我们的软件是一次性收费，每年只需要付极少的网络空间储存费用，就可以使用了，成本大大的降低了。
-                </div>
-        			</div>
+        <div class="css-intro-content2" style="width:1260px;">
+        			<div style="width:955px;margin:0 auto">
+                <div class="css-intro-content2-card">
+          				<div style="height:30%">
+                    <img class="css-intro-content2-card-icon" src="./assets/img/onCloud.png" />
+                    <div class="css-intro-content2-card-name">云端技术</div>
+                  </div>
+          				<div class="css-intro-content2-card-desc extraH">
+                    大部分家具厂家依然停留在纸质媒介的传统传播手段，如图册，折页等，但非常遗憾，这种传统的传播途径明显已经跟不上移动网络时代传播的节奏和脚步，通过搭搭家APP系统的云端技术，网络传播，轻松打通所有客户渠道，所有的经销商，专卖店，只要下载一个软件，就能轻松开始做营销了!
+                  </div>
+          			</div>
+          			<div class="css-intro-content2-card marginLf">
+          				<div style="height:30%">
+                    <img class="css-intro-content2-card-icon" src="./assets/img/onSpace.png" />
+                    <div class="css-intro-content2-card-name">海量空间</div>
+                  </div>
+          				<div class="css-intro-content2-card-desc extraH" style="">
+                    每间家具厂家都有大量的产品图片需要处理，但大部分还是停留在简单的单张图片保存，整理效率和传播效率都非常低下，通过软件后台有序列地管理，大大提高整理效果和产品档案归类，并且能够通过网络瞬间传播，这是纸质媒介没办法达到的效果，而且云端海量空间，再多的图片也不用担心。
+                  </div>
+          			</div>
+          			<div class="css-intro-content2-card marginLf">
+          				<div style="height:30%">
+                    <img class="css-intro-content2-card-icon" src="./assets/img/onUpdate.png" />
+                    <div class="css-intro-content2-card-name">灵活更新</div>
+                  </div>
+          				<div class="css-intro-content2-card-desc" style="height:70%">
+                    每间家具厂，每年都会推出新产品，但是旧图册是没办法瞬间更新的，又只能重新排版，重新印刷，年复一年不断付出高成本的图册费用，软件则不存在这种问题，随时上传，随时更新，随时传播，随时编辑，而且我们的软件是一次性收费，每年只需要付极少的网络空间储存费用，就可以使用了，成本大大的降低了。
+                  </div>
+          			</div>
+                <div class="clearfix"></div>
+              </div>
     		</div>
-        <div class="clear"></div>
+
     	</div>
     	<div class="css-intro">
     		<div class="css-intro-title">
@@ -123,7 +129,7 @@
     		</div>
     		<div class="css-intro-content3" style="width:845px;">
       			<div class="css-intro-content3-card">
-      					<div style="height:50%">
+      					<div style="height:70%">
                     <img class="css-intro-content3-card-icon" src="./assets/img/func_Build.png" />
                     <div class="css-intro-content3-card-name" >快速搭配</div>
             				<div class="css-intro-content3-card-desc">
@@ -163,7 +169,7 @@
                   </div>
       			</div>
       			<!-- clear用作撑开浮动元素div -->
-      					<div class="clear"></div>
+      					<div class="clearfix"></div>
       		</div>
     	</div>
     	<div class="css-shower">
@@ -175,7 +181,7 @@
     		<div class="css-intro2-content" style="width:960px;">
     			<div class="css-intro2-content-title">
               <img class="css-intro2-content-title-icon" src="./assets/img/why_VS.png" />
-    					<div class="clear"></div>
+    					<div class="clearfix"></div>
               <div class="css-intro2-content-title-name" style="text-align:right;padding-right:20px">搭搭家APP</div>
               <div class="css-intro2-content-title-name"style="padding-left:25px;width:50%;float:left;text-align:left">一般图册</div>
     			</div>
@@ -306,7 +312,7 @@
                    佛山市楚翘科技有限公司是一家年轻活力，创意无限，追逐梦想的高新技术型公司，经过多年的发展，旗下拥有搭搭家APP团队，火炬3D设计团队，德宝电商团队，致力于为所有家具厂家和电商客户提供最优质的品牌营销，产品展示与运营咨询等综合型服务。我司旗下每个团队虽然都有不同的功能和目标，但其终极使命都是为了让家具行业得到更好的发展，竭尽全力帮助每一个家具人实现自己心目中的理想！</div>
               <div style=" flex: 1"></div>
 				</div>
-				<div class="clear"></div>
+				<div class="clearfix"></div>
 			</div>
       <div class="css-intro">
 				<div class="css-intro-team">
@@ -380,7 +386,7 @@
   				</div>
   			</div>
   		</div>
-			<div class="clear"></div>
+			<div class="clearfix"></div>
 
       <div class="css-shower2" style="height:120px;" id="Contact">
 				<div class="css-shower2-title IAmContact">
@@ -405,32 +411,40 @@
 				</div>
 			</div>
 
-      <div class="clear"></div>
+      <div class="clearfix"></div>
 
-       <div class="css-footer">
-      		<div class="css-footer-box">
+     <div class="css-footer">
+    		<div class="css-footer-box">
+    			<div style="flex:1"></div>
+          <div style="width:600px;">
+            <div class="css-footer-box-link">搭搭家APP</div>
+      			<div class="css-footer-box-link">火炬3D</div>
+      			<div class="css-footer-box-link">客户案例</div>
+      			<div class="css-footer-box-link">公司简介</div>
+      			<div class="css-footer-box-link" style="border-right:0px solid #62656A">联系我们</div>
+          </div>
+          <div style="flex:1"></div>
+        </div>
+     		<div class="css-footer-box" style=";">
+     			<div style="flex:1"></div>
+           <div>
+             <img src="./assets/img/logoOnFoot.png" />
+     			 </div>
+           <div style="flex:1"></div>
+         </div>
+         <div class="css-footer-box" style=";">
       			<div style="flex:1"></div>
-            <div style="width:600px;">
-              <div class="css-footer-box-link">搭搭家APP</div>
-        			<div class="css-footer-box-link">火炬3D</div>
-        			<div class="css-footer-box-link">客户案例</div>
-        			<div class="css-footer-box-link">公司简介</div>
-        			<div class="css-footer-box-link" style="border-right:0px solid #62656A">联系我们</div>
-            </div>
+            <div>
+
+                版权所有 © 2017 广州汽车集团股份有限公司 粤ICP备17073358号-1
+
+      			 </div>
             <div style="flex:1"></div>
           </div>
-         		<div class="css-footer-box" style=";">
-         			<div style="flex:1"></div>
-               <div>
-                 <img src="./assets/img/logoOnFoot.png" />
-         			 </div>
-               <div style="flex:1"></div>
-             </div>
+    		<div class="clearfix"></div>
+    	</div>
 
-      		<div class="clear"></div>
-      	</div>
-
-  		<div class="clear"></div>
+  		<div class="clearfix"></div>
   </div>
 </template>
 
@@ -456,7 +470,7 @@ export default {
 /*头部*/
 #header-2017{
 	width: 100%;
-	height: 100px;
+	height: 140px;
 	background: #fff;
   position:fixed;
   border-bottom:1px solid #CFCFCF;
@@ -464,18 +478,18 @@ export default {
   z-index:999;
 }
 
-@media screen and (max-width: 1140px) {
+@media screen and (max-width: 1260px) {
 	#header-2017{
-		width: 1140px;
+		width: 1260px;
 	}
 }
 
 .css-login {
 	width: 100%;
   text-align: right;
-  padding: 5px 0px;
+  padding: 10px 0px;
   margin:0 auto;
-  height: 30px;
+  height: 40px;
   background-color: #E1E1E1;
   font-size: 12px;
   color:#666666;
@@ -496,14 +510,13 @@ export default {
     	float: left;
     }
     img{
-    	width: 60px;
-    	margin-top: 6px;
+    	width: 87px;
+    	margin-top: 10px;
     }
   }
   .css-header-center {
     float: right;
     margin-right: 20px;
-    height: 65px;
     ul li{
     	float: left;
     	// margin-top: 23px;
@@ -512,17 +525,18 @@ export default {
     }
 
     ul li a{
+      line-height: 27px;
       display: block;
     	font-size: 16px;
     	color: #606B70;
       text-align: center;
       border-radius: 2px;
-    	padding: 19px 10px;
+    	padding: 35px 10px;
       margin-top:1px;
       margin-left:2px;
       margin-right: 2px;
       margin-bottom:  2px;
-      height:66px;
+      height:97px;
 
       transition: background .3s, color .3s, border .3s;
       -moz-transition: background .3s, color .3s, border .3s; /* Firefox 4 */
@@ -540,78 +554,79 @@ export default {
 }
 /*主体内容部分*/
 .css-body{
-	padding-top: 34px;
-	width: 100%;
-	height: 580px;
-	background: url(./assets/img/bg1.png) no-repeat center center;
+  height: 822px;
+	// background: url(./assets/img/bg1.png)   ;
 	background-size: cover;
-
   border-bottom:40px solid #4FB4D2;
-  .css-body-bg {
-    position:absolute;
-    top:190px;
-    left:55%;
+  .css-body-bg{
+    width: 100%;
+    height: 800px
   }
   .css-body-ad{
   	text-align: left;
     color: white;
     position:absolute;
-    top:280px;
-    left:28%;
+    top:360px;
+    left:250px;
     .css-body-ad-title {
-      font-size: 36px;
+      font-size: 60px;
     }
     .css-body-ad-engtitle {
-      font-size: 19px;
+      font-size: 28px;
     }
     .css-body-ad-desc {
-      font-size: 12px;
-      width: 295px;
+      margin-top: 10px;
+      width: 435px;
+      font-family: '微软雅黑';
+      letter-spacing: 2px;
+      font-weight: lighter;;
+      line-height: 25px;
+      text-align: justify;
+      text-justify: inter-ideograph;/*IE*/
     }
-  }
 
-  .css-body-btn{
-  	text-align: left;
-      color: white;
-      position:absolute;
-      top:420px;
-      left:27%;
-    .css-body-btn-img {
-        float:left;
-    		width: 20px;
-    		margin-left: 35px;
-    		margin-top: 15px;
-    		margin-right: 10px;
-    }
-    .css-body-btn-desc {
-        float:left;
-        width: 70%;
-        font-family: arial;
-        font-size: 12px;
-        margin-top: 2px;
-
-    }
-    a{
-    	display: inline-block;
-    	width: 260px;
-    	height: 56px;
-    	line-height: 54px;
-    	font-size: 18px;
-    	color: #ededff;
-    	background: rgb(48,169,190);
-    	border-radius: 25px;
-    	margin: 0 10px;
-    	box-shadow: 4px 4px rgba(0,0,0,0.1);
+    .css-body-ad-btn{
+    	text-align: left;
+        color: white;
+        position:absolute;
+        top:230px;
+        left:-15px;
+      .css-body-ad-btn-img {
+          float:left;
+      		width: 20px;
+      		margin-left: 35px;
+      		margin-top: 15px;
+      		margin-right: 10px;
+      }
+      .css-body-ad-btn-desc {
+          float:left;
+          width: 70%;
+          font-family: arial;
+          font-size: 12px;
+          margin-top: 2px;
+      }
+      a{
+      	display: inline-block;
+      	width: 260px;
+      	height: 56px;
+      	line-height: 54px;
+      	font-size: 18px;
+      	color: #ededff;
+      	background: rgb(48,169,190);
+      	border-radius: 25px;
+      	margin: 0 10px;
+      	box-shadow: 4px 4px rgba(0,0,0,0.1);
 
 
-    	transition: background .3s, color .3s, border .3s;
-    	-moz-transition: background .3s, color .3s, border .3s; /* Firefox 4 */
-    	-webkit-transition: background .3s, color .3s, border .3s; /* Safari 和 Chrome */
-    	-o-transition: background .3s, color .3s, border .3s; /* Opera */
-    }
-    a:hover{
-      background: rgb(53, 197, 212);
-      color: #ededff;
+      	transition: background .3s, color .3s, border .3s;
+      	-moz-transition: background .3s, color .3s, border .3s; /* Firefox 4 */
+      	-webkit-transition: background .3s, color .3s, border .3s; /* Safari 和 Chrome */
+      	-o-transition: background .3s, color .3s, border .3s; /* Opera */
+      }
+      a:hover{
+        background: rgb(53, 197, 212);
+        color: #ededff;
+      }
     }
   }
 
@@ -644,14 +659,14 @@ export default {
     .css-intro-title-ask {
       float:left;
       border-bottom: 1px solid #4FB4D2;
-      font-size: 24px;
+      font-size: 32px;
       line-height: 45px;
+      letter-spacing: 3px;
     }
   }
   .css-intro-content3 {
       width: 1200px;
       margin: 0 auto;
-      font-size: 12px;
       margin-top: 20px;
       color: #606B70;
 
@@ -661,29 +676,32 @@ export default {
         height:300px;
         .css-intro-content3-card-icon {
           padding: 5px;
-          width: 20%;
-          height: 40px;
+          width: 25%;
+          height: 51px;
         }
         .css-intro-content3-card-name {
-          font-size: 14px;
+          margin: 10px 0px 15px 0px;
+          font-size: 26px;
           color: black;
-          font-weight: bold;
+
         }
         .css-intro-content3-card-desc {
           margin-top: 10px;
           font-family: '宋体';
+          font-size: 16px;
           color: #676767;
+          text-align: justify;
+
         }
       }
       .css-intro-content3-card2 {
         margin-left:35px;
         float:left;
         width:40%;
-        height:300px;
         padding: 15px 0px;
 
         .css-intro-content3-card2-img {
-          height: 97%;
+          height: 370px
         }
       }
       .css-intro-content3-3D {
@@ -780,7 +798,7 @@ export default {
     color: #606B70;
     .css-intro-content2-card {
       float:left;
-      width:210px;
+      width:265px;
       &.marginLf {
         margin-left:80px;
       }
@@ -789,12 +807,17 @@ export default {
         width: 30%;
       }
       .css-intro-content2-card-name {
-        font-size: 14px;
+        font-size: 18px;
         color: black;
+        margin: 10px 0px 15px 0px;
       }
       .css-intro-content2-card-desc {
         font-family: '宋体';
         color: #676767;
+        line-height: 25px;
+        font-size: 14px;
+        text-align: justify;
+        text-justify: inter-ideograph;
         &.extraH {
           height:70%
         }
@@ -802,11 +825,15 @@ export default {
     }
   }
   .css-intro-content {
-  	width: 550px;
+  	width: 750px;
   	margin: 0 auto;
-  	font-size: 14px;
+  	font-size: 18px;
   	color: #676767;
-    text-align: left;
+    text-align: center;
+    letter-spacing: 1px;
+    font-family: '微软雅黑';
+    font-weight: lighter;;
+    margin-top: 30px;
   }
 
   .css-intro-content4 {
@@ -929,6 +956,12 @@ export default {
   }
 }
 
+@media screen and (max-width: 1260px) {
+	.css-intro2{
+		width: 1260px;
+    background-color: red;
+	}
+}
 
 /*介绍栏2*/
 .css-shower2{
